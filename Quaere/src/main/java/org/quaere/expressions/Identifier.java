94dc70cf5ff9ace8e98256af5749639aa32007c3
anoras <anoras@92dcaf01-7c3a-0410-9236-883dd0a17eca>
2007-10-02 21:42:01 +0000

    QUAERE-26 Added argument checking to Identifier

--- Quaere/src/main/java/org/quaere/expressions/Identifier.java
+++ Quaere/src/main/java/org/quaere/expressions/Identifier.java
@@ -6,7 +6,21 @@ public class Identifier extends Expression {
     private final String text;
 
     public Identifier(String text) {
+        validateIdentifier(text);
         this.text = text;
+    }
+    private void validateIdentifier(String text) throws IllegalArgumentException {
+        if (text == null || text.equals("")) {
+            throw new IllegalArgumentException("The identifier cannot be null or an empty string.");
+        }
+        if (!Character.isJavaIdentifierStart(text.charAt(0))) {
+            throw new IllegalArgumentException("The identifier must start with a Java identifier start character.");
+        }
+        for (int i = 1; i < text.length(); i++) {
+            if (!Character.isJavaIdentifierPart(text.charAt(i))) {
+                throw new IllegalArgumentException("The identifier can only contain Java identifier part characters.");
+            }
+        }
     }
 
     public String getText() {

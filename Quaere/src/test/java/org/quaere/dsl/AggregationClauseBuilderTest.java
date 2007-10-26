70e62fe5f1a0aafa10ec6b536947a4a6c7b8701a
anoras <anoras@92dcaf01-7c3a-0410-9236-883dd0a17eca>
2007-10-26 20:13:56 +0000

    Added missing unittests from last revision... My bad..

---
+++ Quaere/src/test/java/org/quaere/dsl/AggregationClauseBuilderTest.java
@@ -0,0 +1,144 @@
+package org.quaere.dsl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.quaere.Queryable;
+import org.quaere.QueryableIterable;
+import org.quaere.expressions.*;
+
+import java.util.Arrays;
+
+public class AggregationClauseBuilderTest {
+    @Test
+    public void canApplyMinOperatorToArray() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("min",new Identifier("n"));
+        Double min = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(1D, min);
+    }
+    @Test
+    public void canApplyMinOperatorToIterable() {
+        Iterable<Integer> numbers = Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2);
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("min",new Identifier("n"));
+        Double min = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(1D, min);
+    }
+    @Test
+    public void canApplyMinOperatorToQueryable() {
+        Queryable<Integer> numbers = new QueryableIterable<Integer>(Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2));
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("min",new Identifier("n"));
+        Double min = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(1D, min);
+    }
+    @Test
+    public void canApplyMinOperatorWithLiteralQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("min",new Identifier("n"));
+        Double min = clauseBuilder.by("n + 1").in(Double.class,numbers);
+        Assert.assertEquals(2D, min);
+    }
+    @Test
+    public void canApplyMinOperatorWithQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("min",new Identifier("n"));
+        Double min = clauseBuilder.by(
+                new PlusOperator(
+                        new Statement(
+                                Arrays.<Expression>asList(new Identifier("n"))
+                        ),
+                        new Statement(
+                                Arrays.<Expression>asList(new Constant(1))
+                        )
+                )
+        ).in(Double.class,numbers);
+        Assert.assertEquals(2D, min);
+    }
+    @Test
+    public void canApplyMaxOperatorToArray() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("max",new Identifier("n"));
+        Double max = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(9D, max);
+    }
+    @Test
+    public void canApplyMaxOperatorToIterable() {
+        Iterable<Integer> numbers = Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2);
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("max",new Identifier("n"));
+        Double max = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(9D, max);
+    }
+    @Test
+    public void canApplyMaxOperatorToQueryable() {
+        Queryable<Integer> numbers = new QueryableIterable<Integer>(Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2));
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("max",new Identifier("n"));
+        Double max = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(9D, max);
+    }
+    @Test
+    public void canApplyMaxOperatorWithLiteralQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("max",new Identifier("n"));
+        Double max = clauseBuilder.by("n + 1").in(Double.class,numbers);
+        Assert.assertEquals(10D, max);
+    }
+    @Test
+    public void canApplyMaxOperatorWithQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("max",new Identifier("n"));
+        Double max = clauseBuilder.by(
+                new PlusOperator(
+                        new Statement(
+                                Arrays.<Expression>asList(new Identifier("n"))
+                        ),
+                        new Statement(
+                                Arrays.<Expression>asList(new Constant(1))
+                        )
+                )
+        ).in(Double.class,numbers);
+        Assert.assertEquals(10D, max);
+    }
+    @Test
+    public void canApplyAverageOperatorToArray() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("average",new Identifier("n"));
+        Double average = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(5D, average);
+    }
+    @Test
+    public void canApplyAverageOperatorToIterable() {
+        Iterable<Integer> numbers = Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2);
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("average",new Identifier("n"));
+        Double average = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(5D, average);
+    }
+    @Test
+    public void canApplyAverageOperatorToQueryable() {
+        Queryable<Integer> numbers = new QueryableIterable<Integer>(Arrays.asList(5, 4, 1, 3, 9, 8, 6, 7, 2));
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("average",new Identifier("n"));
+        Double average = clauseBuilder.by("").in(Double.class,numbers);
+        Assert.assertEquals(5D, average);
+    }
+    @Test
+    public void canApplyAverageOperatorWithLiteralQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("average",new Identifier("n"));
+        Double average = clauseBuilder.by("n + 1").in(Double.class,numbers);
+        Assert.assertEquals(6D, average);
+    }
+    @Test
+    public void canApplyAverageOperatorWithQualifierExpression() {
+        Integer[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2};
+        AggregationClauseBuilder<Double> clauseBuilder = new AggregationClauseBuilderImpl<Double>("average",new Identifier("n"));
+        Double average = clauseBuilder.by(
+                new PlusOperator(
+                        new Statement(
+                                Arrays.<Expression>asList(new Identifier("n"))
+                        ),
+                        new Statement(
+                                Arrays.<Expression>asList(new Constant(1))
+                        )
+                )
+        ).in(Double.class,numbers);
+        Assert.assertEquals(6D, average);
+    }
+}
